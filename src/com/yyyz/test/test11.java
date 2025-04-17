2e76fa3f1c4b89704d777659cd76f1ad833ebc66
yyy <yyy@gmail.com>
2025-04-17 17:43:41 +0800

    测试回滚3

---
+++ src/com/yyyz/test/test11.java
@@ -0,0 +1,29 @@
+package com.yyyz.test;
+
+/**
+ * @Author yzz
+ * @Date 2024/10/31 上午11:13
+ */
+
+public class test11 {
+    public static void main(String[] args) {
+        // 原始字符串
+        String input = "id, count, capacity, data_time, type, create_time, update_time";
+
+        // 将字符串按逗号分割成数组
+        String[] parts = input.split(", ");
+
+        // 使用 StringBuilder 来构建最终的字符串
+        StringBuilder result = new StringBuilder();
+
+        for (String part : parts) {
+            if (result.length() > 0) {
+                result.append(", ");
+            }
+            result.append("\"").append(part).append("\"");
+        }
+
+        // 输出结果
+        System.out.println(result.toString());
+    }
+}
